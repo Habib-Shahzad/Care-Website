040f0113e03fbf2442e28e2b72ac58c468ada6c8
Hammad-Ahmed-A <ha05890@st.habib.edu.pk>
2022-08-12 14:25:03 +0500

    updated a few conflicted pages

--- client/src/pages/ourActivities/OurActivities.js
+++ client/src/pages/ourActivities/OurActivities.js
@@ -3,6 +3,87 @@ import './OurActivities.scss';
 
 
 function OurActivities(props) {
+    const actives = [
+        {
+            name: 'Fehmida’s Senior Care Home',
+            date: '30 June 2022',
+            description:'Our team visited Fehmida’s Senior Care Home, another old age home and spent the day there with senior citizens, doing activities and having lunch as well as listening to all difficulties they’re going through. We wanted this to be our last project before summer break.',
+            image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+        },
+        {
+            name: 'World Blood Donor Day',
+            date: '14 June 2022',
+            description:'A blood donation drive to take place in the Blood Bank where students across campus are encouraged to come and donate blood. This drive is arranged in order to gather blood for the blood bank as well as patients in Dow Hospital, Ojha.',
+            image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+        },
+        {
+            name: 'Brain Tumour Awareness Seminar  - Dar Ul Sukoon',
+            date: '6 June 2022',
+            description:'A session was conducted by our team at Dar Ul Sukoon to train the staff on brain tumours and management as they’ve children with disabilities. It was an interactive session with many further follow ups. This session was a part of our recently launched Community Outreach Program which targets educating our society on topics not openly discussed such as rape, self harm, hygiene etc. The program also works in accordance with Dow’s SDG goals.',
+            image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+        },
+        {
+            name: 'Dar Ul Sukoon - Old Age Home',
+            date: '4 June 2022',
+            description:'Our team visited Dar Ul Sukoon old age home to spend a day with them. They conducted activities, heard their stories, arranged lunch, flowers and cards for them. At the end an amount of 25000/- was also donated. 120+ seniors were targeted during this visit.',
+            image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+        },
+        {
+            name: 'First Year Guide - Blood Module',
+            date: '24 April 2022',
+            description:'An online seminar was conducted by 2nd year students who shared their opinions and study techniques for the first year students throughout Pakistan.',
+            image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+        },
+        {
+            name: 'Girls Chaand Raat ',
+            date: '24 April 202',
+            description:'Earings, bangles, henna were arranged on campus for girls specially in hostels to make Eid easier in the heat of Ramadan.',
+            image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+        },
+        {
+            name: 'Khayaal Campaign',
+            date: '3 April 2022 - 3 May 2022',
+            description:'This has been the biggest campaign of the year till now. A total of 9 lakh rupees were utilised. Khayal campaign was to provide relief to the underprivileged in the month of Ramadan. We conducted multiple street iftars as well as ration drives across Karachi. We also donated to many small organisations that were in need as well as individual needy cases that were brought up. During Khayaal Campaign CARE fed iftar to 1000+ people as well as rationing to 100+ families across Karachi.',
+            image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+        },
+        {
+            name: 'Ramadan Ambassador Programme',
+            date: '3 April 2022 - 3 May 2022',
+            description:'This program was launched to gather donations for our Khayal Campaign. This ambassadorship program was opened to students from different universities across Karachi. Their tasks were to collect donations and spread awareness of our goals. They were divided in groups with CARE members as group leaders. Each ambassador was awarded a certificate by CARE upon accomplishment of tasks.100+ students across universities in Karachi took part, this also increased CARE’s public reach.',
+            image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+        },
+        {
+            name: 'Research Insiders - Introduction to Research Seminar',
+            date: '25 March 2022 ',
+            description:'Our Research department conducted an Online workshop for students across Pakistan with Dr.Asif Qureshi as our host who guided the audience on how to start with their research. Our participant list exceeded 250+ students.',
+            image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+        },
+        {
+            name: 'Culture Day',
+            date: '22 March 2022',
+            description:'A performance about all provinces and cultures as well as food stalls were arranged by our team to celebrate Pakistan Day and our culture with the entire student body at Dow Ojha',
+            image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+        },
+        {
+            name: 'First Year Guide to Med School',
+            date: '14 February 2022',
+            description:'An online seminar was held where a panel of 2nd year students shared their resources and study methods to the new batch. This seminar targeted students around Pakistan.',
+            image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+        },
+        {
+            name: 'Breast Cancer Awareness Session',
+            date: '6 October 2021 ',
+            description:'A seminar in the NIDE building was conducted followed up by a question session and a checkup with the assistance of doctors. This seminar was targeting all lower class women who face issues but have no knowledge.',
+            image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+        },
+        {
+            name: 'Bake Sale - Delicious Donations',
+            date: '20 September 2021',
+            description:'A bake sale was conducted in Dow Ojha’s Cafeteria. The aims were to collect donations for STS as well as promote small home business owners',
+            image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+        },
+
+    ]; 
 
     return (
         <div className='text-center our-aims-main front'>
@@ -10,82 +91,24 @@ function OurActivities(props) {
                 <h1 className='pink-text'>
                     The Work We Have Done
                 </h1>
-
-                <h3 className='pink-text'>
-                    World Blood Donor Day
-                </h3>
-                <p >
-                    CARE takes priority for various
-                    fundraising projects organized by the
-                    Department of Events, Campaigning, and
-                    Outreach (ECO). Most of these events are
-                    conducted for the students of Dow
-                    University of Health Sciences, with 100%
-                    of the profits directed towards use in
-                    future collaboration and donation drives,
-                    and welfare projects initiated by CARE.
-                </p>
-                <h3 className='pink-text'>
-                    Awareness Campaigns
-                </h3>
-                <p >
-                    In a country with limited access to higher
-                    education, and a literacy rate of under
-                    60% - it is our aim to spread awareness
-                    regarding important W.H.O. monthly
-                    topics, including World Blood Donor Day, World Lung Cancer Day, Breast Cancer
-                    Awareness Month, and more. Using these
-                    campaigns, important information
-                    regarding the nature of disease, etiology
-                    of disease, prevalence of disease, and
-                    treatment of disease will be shared with
-                    the public via CARE.
-                </p>
-                <h3 className='pink-text'>
-                    Bringing Social Changes
-                </h3>
-                <p >
-                    stigmas and issues, which is the top
-                    cause of mental health & self-esteem
-                    issues within the youth of the country.
-                    CARE has launched the “Community
-                    Outreach” program, which aims to create
-                    a social change and address various
-                    stigmas within our culture, including
-                    racism, bullying, child abuse, depression,
-                    and more. These sessions will be
-                    delivered to school, college, and
-                    university students across Karachi.
-                </p>
-                <h3 className='pink-text'>
-                    Medical Development and Research
-                </h3>
-                <p >
-                    CARE’s highly skilled Department of
-                    Research will consistently work towards
-                    writing in medical research articles, and
-                    aims for publications within well-known
-                    journals of the medical industry. The
-                    research will focus on epidemiology,
-                    community medicine, and public health
-                    in order to provide a better
-                    understanding regarding disease
-                    prevalence within our community.
-                </p>
-                <h3 className='pink-text'>
-                    Collaborations:
-                </h3>
-                <p >
-                    CARE actively seeks to participate with
-                    other welfare organizations, or external
-                    organizations, companies, or businesses
-                    with alike interests. Collectively, we can
-                    achieve a better tomorrow.
-                </p>
-
             </div>
-        </div>
-
+            {
+            actives.map((active, index) => {
+                return (
+                    <div className='actives-card'>
+                        <div className='actives-image'>
+                            <img src={active.image} alt={active.name} style={{'width': '25rem'}} />
+                        </div>
+                        <div className='actives-details'>
+                            <h2>{active.name}</h2>
+                            <h3>{active.date}</h3>
+                            <p>{active.description}</p>
+                        </div>
+                    </div>
+                )
+            })
+            }
+            </div>
     )
 }
 
--- client/src/pages/patientWelfare/PatientWelfare.js
+++ client/src/pages/patientWelfare/PatientWelfare.js
@@ -4,19 +4,35 @@ import './PatientWelfare.scss';
 
 
 function PatientWelfare(props) {
+        const PatientBlogs =[
+            {
+                text:"CAREs Patient Welfare Programme was established with the vision of assisting every underprivileged patient in the finest and fastest way possible. The programme was started by first-year MBBS students in the DOW Ojha campus after seeing a lack of support for those who need it the most. Many disadvantaged patients feel frustrated and helpless during their hospital visits. The declining health of their loved ones compounded by their financial strains and the confusing health care system sends them into a spiral of depression. We aim to provide holistic care to our patients by not only assisting them financially but also guiding them during their time in the hospital to find the answers they are so desperately in search of. From assisting young adults with an early cancer diagnosis to providing life-saving hepatitis medication and even organizing blood donation drives for bone marrow transplant patients, the programme strives to help any and all who reach out for help. We primarily focus on providing financial support for diagnostic tests in DOWs Radiology and Blood lab department. So donate now to join us in this journey of bringing smiles to the needy during their toughest time!",
+                image:'public/ambass-image.jpg'
+            }
+        ];
     return (
         <div className='text-center patient-welfare-head front'>
             <div className='front'>
                 <h1 className='pink-text'>
                     Care Patient Welfare
                 </h1>
-                <p >
-                    The team responsible for the welfare activity within the Dow University Hospital attached to the Dow International Medical College, Karachi. This is one of CARE’s latest projects that has observed noteworthy progress. It is also capable of handling the welfare cases in other government hospitals of Karachi, including Civil Hospital Karachi. The members of the team manually mediate any welfare case brought to CARE using a robust verification system that estimates the financial neediness of the patient,
-                    through a series of discussions, interviews, and questionnaire
-                </p>
             </div>
+            {
+                PatientBlogs.map((blog, index) => {
+                    return (
+                        <div className='blog-card'>
+                            <div className='blog-image'>
+                                <img src={blog.image} alt={blog.name} style={{'width': '25rem'}} />
+                            </div>
+                            <div className='blog-details'>
+                                <h3>{blog.name}</h3>
+                                <p>{blog.text}</p>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
-
 export default PatientWelfare;
