d8fcbf8d669a6f07c4b19de49da1f659ac41fbbb
Hammad-Ahmed-A <ha05890@st.habib.edu.pk>
2022-08-13 21:30:21 +0500

    updated  members and leaders in team page

--- client/src/pages/ourTeam/OurTeam.js
+++ client/src/pages/ourTeam/OurTeam.js
@@ -23,18 +23,52 @@ function OurTeam(props) {
                     text: 'Gen Sec',
                     image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
                 },
-
+                {
+                    name: 'Muhammad Ali Amir',
+                    text: 'Director of Research and Patient Welfare',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                {
+                    name: 'Rahmah Ashar Sakrani',
+                    text: 'Director of Finance',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                {
+                    name: 'Namrata Panjwani',
+                    text: 'Director of Public Relations and Ambassadors',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },         
+                {
+                    name: 'Abiha Khurram',
+                    text: 'Director of Media and Marketing',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                {
+                    name: 'Rida Batool',
+                    text: 'Director of World Health Organisation',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                {
+                    name: 'Moiz Noman',
+                    text: 'Director of Event Planning',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                }
             ]
         },
 
 
         {
-            name: 'IT department',
+            name: 'Department Of Public Relations',
 
             members: [
                 {
-                    name: 'Mufize Firoz Vohra',
+                    name: 'Ahmad Anwar',
                     text: 'Head of Public Relations',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                {
+                    name: 'Palwasha Kakkar',
+                    text: 'Member',
                     image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
                 },
                 {
@@ -42,36 +76,265 @@ function OurTeam(props) {
                     text: 'Member',
                     image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
                 },
+                {
+                    name: 'Qazi Akhtar',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+
+
 
             ]
         },
 
         {
-            name: 'Public Relations',
+            name: 'Department of Research and Development',
 
             members: [
                 {
-                    name: 'Hammad Ahmad',
+                    name: 'Muhammad Areeb Syed',
+                    text: 'Head of Research and Development',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                {
+                    name: 'Bilal Syed',
                     text: 'Head of Public Relations',
                     image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
                 },
                 {
-                    name: 'Hammad Ahmad',
+                    name: 'Tayyab Zahoor',
                     text: 'Head of Public Relations',
                     image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
                 },
                 {
-                    name: 'Hammad Ahmad',
-                    text: 'Head of Public Relations',
+                    name: 'Yumna Khabir',
+                    text: 'Member',
                     image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
                 },
                 {
-                    name: 'Mufize Firoz Vohra',
+                    name: 'Reeha Amjad',
                     text: 'Member',
                     image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
                 },
             ]
         },
+        {
+            name: 'Department of Patient Welfare',
+
+            members: [
+                { 
+                    name: 'Mahavash Nadeem',
+                    text: 'Head of Patient Welfare',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Syed Haidar Jalal',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Mahnoor Rehan',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+            ]
+        },
+        {
+            name: 'Department of World Health Organisation',
+
+            members: [
+                { 
+                    name: 'Unknown',
+                    text: 'Head of World Health Organisation',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Haadya Khan',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Syeda Javeria',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Zara Jamil',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+            ]
+                
+        },
+        {
+            name: 'Department of Community Outreach',
+
+            members: [
+                { 
+                    name: 'Moiz Noman',
+                    text: 'Head of Community Outreach',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Saljok Ali',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Arfa Khan',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Jenelle Alvares',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Zara Ahmed Qureshi',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+
+            ]        
+        },
+        {
+            name: 'Department of Event Planning',
+
+            members: [
+                { 
+                    name: 'Haniah Mahboob',
+                    text: 'Head of Event Planning',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Abiha Raza',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Atiqa Fatima',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Ahsan Zaheer',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Khubaib Basit',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Anusha Amir',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Ali Rizvi',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Areeka Irfan',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+            ]
+
+        },
+        {
+            name: 'Department of Finance',
+
+            members: [
+                { 
+                    name: 'Unknown',
+                    text: 'Head of Finance',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Rana Zargham Asif',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Muhammad Haseeb Shoaib',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Muhammad Azzam',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+            ]
+
+        },
+        {
+            name: 'Department of Media and Marketing',
+
+            members: [
+                { 
+                    name: 'Areeba Nisa',
+                    text: 'Head of Media and Marketing',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Fahad Farooq',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Arooba Sheikh',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Yasir Raza',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Maryum Mubbashir',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Anas Aijaz',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+            ]
+        },
+        {
+            name: 'Department if Human Resources',
+
+            members: [
+                { 
+                    name: 'Syed Wijdan Ali',
+                    text: 'Head of Human Resources',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Eesha Nadeem',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Abdur Rehman',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+                { 
+                    name: 'Khizar Saeed',
+                    text: 'Member',
+                    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+                },
+            ]
+                
+        }
 
     ];
 
