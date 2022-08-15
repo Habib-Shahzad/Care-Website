fe28ebde527fe93ba029032732d9bf9f636dfbcf
Hammad-Ahmed-A <ha05890@st.habib.edu.pk>
2022-08-15 13:55:04 +0500

    minor updates

--- client/src/pages/patientWelfare/PatientWelfare.js
+++ client/src/pages/patientWelfare/PatientWelfare.js
@@ -34,13 +34,11 @@ function PatientWelfare(props) {
             }
             <div className='text-center blog-start front'>
                 <h3 className="pink-text">
-                    Start a Blog
+                    Start a Bloghh
                 </h3>
                 <p>
                     Many disadvantaged patients feel frustrated and helpless during their hospital visits. The declining health of their loved ones compounded by their financial strains and the confusing health care system sends them into a spiral of depression. We aim to provide holistic care to our patients by not only assisting them financially but also guiding them during their time in the hospital to find the answers they are so desperately in search of. 
-
                     From assisting young adults with an early cancer diagnosis to providing life-saving hepatitis medication and even organizing blood donation drives for bone marrow transplant patients, the programme strives to help any and all who reach out for help. We primarily focus on providing financial support for diagnostic tests in DOWs Radiology and Blood lab department.
-
                     So donate now to join us in this journey of bringing smiles to the needy during their toughest time!
                 </p>
             </div>
