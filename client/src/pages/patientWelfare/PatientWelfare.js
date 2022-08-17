14dfaebcd6e9a425433d905f89eb7775766b7452
Habib Shahzad <60187677+hs05888@users.noreply.github.com>
2022-08-17 23:11:57 +0500

    Patient Welfare page design updates

--- client/src/pages/patientWelfare/PatientWelfare.js
+++ client/src/pages/patientWelfare/PatientWelfare.js
@@ -1,48 +1,83 @@
 import React from "react";
-// import { Button, Container, Row, Col } from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
 import './PatientWelfare.scss';
 
 
 function PatientWelfare(props) {
-        const PatientBlogs =[
-            {
-                text:"CAREs Patient Welfare Programme was established with the vision of assisting every underprivileged patient in the finest and fastest way possible. The programme was started by first-year MBBS students in the DOW Ojha campus after seeing a lack of support for those who need it the most. Many disadvantaged patients feel frustrated and helpless during their hospital visits. The declining health of their loved ones compounded by their financial strains and the confusing health care system sends them into a spiral of depression. We aim to provide holistic care to our patients by not only assisting them financially but also guiding them during their time in the hospital to find the answers they are so desperately in search of. From assisting young adults with an early cancer diagnosis to providing life-saving hepatitis medication and even organizing blood donation drives for bone marrow transplant patients, the programme strives to help any and all who reach out for help. We primarily focus on providing financial support for diagnostic tests in DOWs Radiology and Blood lab department. So donate now to join us in this journey of bringing smiles to the needy during their toughest time!",
-                image:  'public/ambass-image.jpg'
-            }
-        ];
+    const PatientBlogs = [
+        {
+            name: 'Patient Blog 1',
+            text: "CAREs Patient Welfare Programme was established with the vision of assisting every underprivileged patient in the finest and fastest way possible. The programme was started by first-year MBBS students in the DOW Ojha campus after seeing a lack of support for those who need it the most. Many disadvantaged patients feel frustrated and helpless during their hospital visits. The declining health of their loved ones compounded by their financial strains and the confusing health care system sends them into a spiral of depression. We aim to provide holistic care to our patients by not only assisting them financially but also guiding them during their time in the hospital to find the answers they are so desperately in search of. From assisting young adults with an early cancer diagnosis to providing life-saving hepatitis medication and even organizing blood donation drives for bone marrow transplant patients, the programme strives to help any and all who reach out for help. We primarily focus on providing financial support for diagnostic tests in DOWs Radiology and Blood lab department. So donate now to join us in this journey of bringing smiles to the needy during their toughest time!",
+            images: ['https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60', 'https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg'],
+        },
+        {
+            name: 'Patient Blog 2',
+            text: "CAREs Patient Welfare Programme was established with the vision of assisting every underprivileged patient in the finest and fastest way possible. The programme was started by first-year MBBS students in the DOW Ojha campus after seeing a lack of support for those who need it the most. Many disadvantaged patients feel frustrated and helpless during their hospital visits. The declining health of their loved ones compounded by their financial strains and the confusing health care system sends them into a spiral of depression. We aim to provide holistic care to our patients by not only assisting them financially but also guiding them during their time in the hospital to find the answers they are so desperately in search of. From assisting young adults with an early cancer diagnosis to providing life-saving hepatitis medication and even organizing blood donation drives for bone marrow transplant patients, the programme strives to help any and all who reach out for help. We primarily focus on providing financial support for diagnostic tests in DOWs Radiology and Blood lab department. So donate now to join us in this journey of bringing smiles to the needy during their toughest time!",
+            images: ['https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60', 'https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg'],
+        },
+        {
+            name: 'Patient Blog 3',
+            text: "CAREs Patient Welfare Programme was established with the vision of assisting every underprivileged patient in the finest and fastest way possible. The programme was started by first-year MBBS students in the DOW Ojha campus after seeing a lack of support for those who need it the most. Many disadvantaged patients feel frustrated and helpless during their hospital visits. The declining health of their loved ones compounded by their financial strains and the confusing health care system sends them into a spiral of depression. We aim to provide holistic care to our patients by not only assisting them financially but also guiding them during their time in the hospital to find the answers they are so desperately in search of. From assisting young adults with an early cancer diagnosis to providing life-saving hepatitis medication and even organizing blood donation drives for bone marrow transplant patients, the programme strives to help any and all who reach out for help. We primarily focus on providing financial support for diagnostic tests in DOWs Radiology and Blood lab department. So donate now to join us in this journey of bringing smiles to the needy during their toughest time!",
+            images: ['https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60', 'https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg'],
+        }
+    ];
+
     return (
-        <div className='text-center patient-welfare-head front'>
-            <div className='front'>
-                <h1 className='pink-text'>
-                    Care Patient Welfare
-                </h1>
+        <div className='pw-page'>
+
+            <div className="margin-global-top-3" />
+
+            <div className='text-center pw-text-area front'>
+                <div className='box'>
+                    <h1 className='pink-text'>
+                        Care Patient Welfare
+                    </h1>
+                    <p>
+                        Many disadvantaged patients feel frustrated and helpless during their hospital visits. The declining health of their loved ones compounded by their financial strains and the confusing health care system sends them into a spiral of depression. We aim to provide holistic care to our patients by not only assisting them financially but also guiding them during their time in the hospital to find the answers they are so desperately in search of.
+                        From assisting young adults with an early cancer diagnosis to providing life-saving hepatitis medication and even organizing blood donation drives for bone marrow transplant patients, the programme strives to help any and all who reach out for help. We primarily focus on providing financial support for diagnostic tests in DOWs Radiology and Blood lab department.
+                        So donate now to join us in this journey of bringing smiles to the needy during their toughest time!
+                    </p>
+                </div>
             </div>
+
+            <div className="margin-global-top-5" />
+
             {
                 PatientBlogs.map((blog, index) => {
                     return (
-                        <div className='blog-card'>
-                            <div className='blog-image'>
-                                <img src={blog.image} alt={blog.name} style={{'width': '25rem'}} />
-                            </div>
+                        <div className='blog-card text-center'>
                             <div className='blog-details'>
-                                {/* <h3>{blog.name}</h3> */}
-                                <p>{blog.text}</p>
+                                <h3 className="pink-text">{blog.name}</h3>
+                                <div className='blog-image'>
+                                    <Carousel interval={2000 + (index * 100)} fade>
+                                        {
+                                            blog.images.map((image, key) => {
+                                                return (
+                                                    <Carousel.Item
+                                                        key={key}
+                                                    >
+                                                        <img
+                                                            className="d-block w-100"
+                                                            src={image}
+                                                            alt={`${blog.name}`}
+                                                        />
+                                                    </Carousel.Item>
+                                                )
+                                            })
+                                        }
+
+                                    </Carousel>
+                                </div>
+                                <div className="blog-text">{blog.text}</div>
                             </div>
+
                         </div>
                     )
                 })
             }
-            <div className='text-center blog-start front'>
-                <h3 className="pink-text">
-                    Start a Bloghh
-                </h3>
-                <p>
-                    Many disadvantaged patients feel frustrated and helpless during their hospital visits. The declining health of their loved ones compounded by their financial strains and the confusing health care system sends them into a spiral of depression. We aim to provide holistic care to our patients by not only assisting them financially but also guiding them during their time in the hospital to find the answers they are so desperately in search of. 
-                    From assisting young adults with an early cancer diagnosis to providing life-saving hepatitis medication and even organizing blood donation drives for bone marrow transplant patients, the programme strives to help any and all who reach out for help. We primarily focus on providing financial support for diagnostic tests in DOWs Radiology and Blood lab department.
-                    So donate now to join us in this journey of bringing smiles to the needy during their toughest time!
-                </p>
-            </div>
 
+            <br />
+            <br />
         </div>
     )
 }
